932c417dbbf6ee8910cfa07cb60e25c286f5a9bc
josuecorea5 <48955494+josuecorea5@users.noreply.github.com>
2020-12-15 15:03:52 -0600

    Eliminando Config

--- config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  mongoUri:
-    "mongodb+srv://dp_user01:tDMi6SWuDBzP8LF@clasemongodb.vuovb.mongodb.net/ConexionMongo?retryWrites=true&w=majority",
-  PORT: process.env.PORT || 3000,
-};
